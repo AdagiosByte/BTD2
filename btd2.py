4249cb54de70e2d31c1c74bd8a59632cbd497b6b
AdagiosByte <43786133+AdagiosByte@users.noreply.github.com>
2021-10-26 22:30:09 -0400

    Add files via upload
    Features added:
    - Can now select and place boomerangs
    - Program ends at difficulty select screen
    - Fixed bugs

--- btd2.py
+++ btd2.py
@@ -1,37 +1,47 @@
 import pyautogui
 import random
+import time
 
 #difficulties
 def easy():
-    if i[2] > 250:
-            pyautogui.click()
-            placedTower = True
+    if i[2] > 250 and i[0] < 200:
+        pyautogui.click()
+        return True
+    elif i[0] == 255 and i[1] == 255 and i[2] == 255:
+        return True
+    
+    
 def medium():
     if i[0] > 100 and i[1] > 100 and i[2] > 100:
-            pyautogui.click()
-            placedTower = True
+        pyautogui.click()
+        return True
+    return False
 def hard():
     if i[0] > 100 and i[1] > 100 and i[2] > 100:
-            pyautogui.click()
-            placedTower = True
+        pyautogui.click()
+        return True
+    return False
 
 #towers
 def dart():
     pyautogui.click(780,200) # buy dart monkey
 def cannon():
     pyautogui.click(980,200) # buy cannon
+def boomerang():
+    pyautogui.click(900,250) # buy boomerang
 
 gameOver = False
-placedTower = False
+placedTower = True
 
-tower = {0:dart, 1:cannon}
+
+tower = {0:dart, 1:cannon, 2:boomerang}
 stage = {0:easy, 1:medium, 2:hard}
 
 #determine difficulty selected
 a = pyautogui.screenshot()
 i = a.getpixel((100,100))
 
-if i[2] == 255:
+if i[2] >200:
     difficulty = 0
 elif i[2] > 55:
     difficulty = 1
@@ -40,15 +50,15 @@ else:
 
 while gameOver == False:
     
-    tower[random.randint(0,1)]() # select tower
+    tower[random.randint(0,2)]() # select tower
     while placedTower == False:
         
         
         #check for game over (win or loose)
-        pyautogui.moveTo(440,300)
-        x,y = pyautogui.position()
-        i = a.getpixel((x,y))
-        if i[1] == 102 or i[1] == 103: 
+        pyautogui.moveTo(440,170)
+        a = pyautogui.screenshot()
+        i = a.getpixel((440,170))
+        if i[0] == 30: 
             gameOver = True
             break
         
@@ -59,18 +69,16 @@ while gameOver == False:
         
             
         #place tower
-        stage[difficulty]()
+        placedTower = stage[difficulty]()
         
         
-        
-    pyautogui.click(800,500) # buy left upgrade
+    pyautogui.moveTo(800,500) # buy left upgrade
+    time.sleep(.1)
+    pyautogui.click()
     pyautogui.click(900,500) # buy right upgrade
-    pyautogui.click(900,700) # press Start Round
+    pyautogui.moveTo(900,700) # press Start Round
+    pyautogui.click()
     placedTower = False
-    
-    
-    
-    
     
     
     #pyautogui.click(780,250) # buy tack tower
